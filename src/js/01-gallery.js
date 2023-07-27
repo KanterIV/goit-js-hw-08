8a8e739d92ee2c8db629e8953a23b11930130496
KanterIV <Kanterigor@gmail.com>
2023-07-27 17:04:50 +0200

    fix v 0.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 const galleryItemEl = galleryItems
