1ef105cd41347da7b5ae4af5c10319c28a0bf213
KanterIV <Kanterigor@gmail.com>
2023-07-29 09:39:52 +0200

    fix v 0.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,9 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
